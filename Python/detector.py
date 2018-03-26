522c19c6d6db9842e67ef14c557cbcd1ca2690a0
Sushant <sushanta1996@gmail.com>
2018-03-26 22:06:53 +1100

    img center and stuff,

--- Python/detector.py
+++ Python/detector.py
@@ -1,6 +1,7 @@
 import cv2
 import logging as log
 from time import sleep
+import sys
 def main():
     print('hi')
 
@@ -50,16 +51,26 @@ class Detector:
                 minNeighbors=5,
                 minSize=(30, 30)
             )
+            
               #Draw a rectangle around the faces
+            
+            # Font= cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, hscale= FontSize, vscale= 1.0, shear=0, thickness=1, lineType=8)
             for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (200, 200, 90), 2)
+                #cv2.rectangle(frame, (x, y), (x+w, y+h), (120, 2, 0), 2)
+                a = int(x + (w/2))
+                b = int(y + (h/2))
+                # eye = [int(a-(a/2)), int(b-(b/2))]
+                #draw a TINY rectangle at the centre of the face so it look like a dot.
+                x = cv2.rectangle(frame, (a, b), (a, b), (500, 500, 0), 3)
+                print(a,',',b)
 
             if len(faces) > 0:
-                print('face')
-                
+                # print(frame)
+                print('SHape', frame.shape)
 
             # Display the resulting frame
             cv2.imshow('Detections', frame)
+            
             #add a event listener to close the frame
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 sys.exit()
