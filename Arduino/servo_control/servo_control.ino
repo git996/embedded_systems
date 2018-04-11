a789455c4f896ce85c0084630b92040c1f61ca2b
Sushant <sushanta1996@gmail.com>
2018-04-11 18:44:10 +1000

    servo

--- Arduino/servo_control/servo_control.ino
+++ Arduino/servo_control/servo_control.ino
@@ -1,29 +1,29 @@
-// the setup function runs once when you press reset or power the board
-int led = 8;
-int state;
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(led, OUTPUT);
-  Serial.begin(9600);
-}
-
-// the loop function runs over and over again forever
-void loop() {
-
-}
-
-void serialEvent(){
+#include <Servo.h>
+Servo serx;
+int x  = 0;
+void setup(){
   
-    if(Serial.available() > 0){
-      if(Serial.peek() == 't'){
-        Serial.read();
-        state = Serial.parseInt();
-        Serial.read();
-        digitalWrite(led, state);
-      }
-      while(Serial.available() > 0){
-        Serial.read();
-      }
-    }
+  serx.attach(11);
+  sery.attach(10)
+  serx.write(0);
+  serx.write(0);
+  delay(2000);
 }
+void loop(){
+ 
+  if (x < 179){
+    serx.write(x);
+    sery.write(x);
+    delay(500);
+    x = x+10;
+  }
+  if (x > 179){
+    x = 0;
+  }
+   
+ }
+
+
+  
+  
 
